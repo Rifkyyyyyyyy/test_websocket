b0fe5839488ddbf969bfb31278f620373cf3b3c1
Rifkyyyyyyyy <zayx1231@gmail.com>
2025-03-02 07:44:11 +0700

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws')
 const PORT = 3000;
 const wss = new WebSocket.Server({ port: PORT })
-const server = http
+
 wss.on('connection', ws => {
     ws.on('message', message => {
         console.log(`Received message => ${message}`)
