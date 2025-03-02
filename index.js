4e5e1d0a10e48b16b5215e85c2a25ffb411fa052
Rifkyyyyyyyy <zayx1231@gmail.com>
2025-03-02 07:42:11 +0700

    test

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-const http = require('http');
-const WebSocket = require('ws');
-
+const WebSocket = require('ws')
 const PORT = 3000;
-const server = http.createServer(); // Buat server HTTP
+const wss = new WebSocket.Server({ port: PORT })
+const server = http
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        console.log(`Received message => ${message}`)
+    })
+    setInterval(function () { ws.send('Hello! Message From Server!!') }, 1000);
 
-// Buat WebSocket server menggunakan HTTP server
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-
-    ws.on('message', (message) => {
-        console.log(`Received message => ${message}`);
-    });
-
-    // Kirim pesan ke client setiap 1 detik
-    const interval = setInterval(() => {
-        if (ws.readyState === WebSocket.OPEN) {
-            ws.send('Hello! Message From Server!!');
-        }
-    }, 1000);
-
-    // Hentikan interval jika koneksi ditutup
-    ws.on('close', () => {
-        console.log('Client disconnected');
-        clearInterval(interval);
-    });
-});
-
-// Jalankan server di port 3000
-server.listen(PORT, () => {
-    console.log(`WebSocket server running on ws://localhost:${PORT}`);
-});
+})
